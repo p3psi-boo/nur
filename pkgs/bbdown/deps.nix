dcf4e2d629225851f9d53b7c2a3b349b73333f88
bubu <bubu2131@duck.com>
2026-05-07 12:52:30 +0000

    fix bbdown: remove illink.tasks conflicting with SDK

--- pkgs/bbdown/deps.nix
+++ pkgs/bbdown/deps.nix
@@ -2,7 +2,6 @@
 { fetchNuGet }: [
   (fetchNuGet { pname = "google.protobuf"; version = "3.28.3"; sha256 = "sha256-jiA/FeYEEk/u9O1gtdnOzatym+/uHyaRJSdp34TOb1o="; })
   (fetchNuGet { pname = "grpc.tools"; version = "2.67.0"; sha256 = "sha256-ms/lbWwb9UuJHNl3T5X2mAulCHhQ3tEiqRLWBfUYoV0="; })
-  (fetchNuGet { pname = "microsoft.net.illink.tasks"; version = "9.0.15"; sha256 = "sha256-Nw2S2tfktJBMNZUFlloaJ/85URM6fm0bIF8LVb0XWNY="; })
   (fetchNuGet { pname = "qrcoder"; version = "1.6.0"; sha256 = "sha256-2Ev/6d7PH6K4dVYQQHlZ+ZggkCnDtrlaGygs65mDo28="; })
   (fetchNuGet { pname = "sharpziplib"; version = "1.4.2"; sha256 = "sha256-/giVqikworG2XKqfN9uLyjUSXr35zBuZ2FX2r8X/WUY="; })
   (fetchNuGet { pname = "system.commandline"; version = "2.0.0-beta4.22272.1"; sha256 = "sha256-zSO+CYnMH8deBHDI9DHhCPj79Ce3GOzHCyH1/TiHxcc="; })
